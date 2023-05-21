a612e440b92f520da4ff3834a9e8c68a0a91a077
Christian Flores <c1flores@ucsd.edu>
2023-05-20 20:43:31 -0700

    Add check_code() function

--- Assets/game.py
+++ Assets/game.py
@@ -34,5 +34,26 @@ def guess_code():
                 break
 
 # Compare the guess
+def check_code(guess, real_code):
+    color_counts = {}
+    correct_position = 0
+    incorrect_position = 0
+
+    for color in real_code:
+        if color not in color_counts:
+            color_counts[color] = 0
+        color_counts[color] += 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color == real_color:
+            correct_position += 1
+            color_counts[guess_color] -= 1
+        
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_counts and color_counts[guess_color] > 0:
+            incorrect_position -= 1
+            color_counts[guess_color] -= 1
+
+    return correct_position, incorrect_position
 
 # Tie the game together - provide user with feedback on scoring
