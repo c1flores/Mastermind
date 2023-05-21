01ea5eefdb3ea8d8bc1e7c150d01e560ea0eb413
Christian Flores <c1flores@ucsd.edu>
2023-05-20 20:25:34 -0700

    Fix typos in generate_code() function and create guess_code() function

--- Assets/game.py
+++ Assets/game.py
@@ -1,12 +1,12 @@
 # This module implements pseudo-random number generators for various distributions
 import random
 
-# Constants Defining all the colors that we'll be using, number user attempts, and code length
+# Constants Defining all the colors that we'll be using, number of user attempts, and code length
 COLORS =  ["R", "G", "B", "Y", "w", "O"]
 TRIES = 10
 CODE_LENGTH = 4
 
-# Generate a 3 color random code
+# Generate a 4 color random code
 def generate_code():
     code = []
 
@@ -15,7 +15,23 @@ def generate_code():
         code.append(color)
     
     return code
+
 # Make the user guess the code
+def guess_code():
+    while True:
+        guess = input("Guess:").upper().split(" ")
+
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} colors.")
+            continue
+
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color: {color}. Try again")
+            
+                break
+            else:
+                break
 
 # Compare the guess
 
